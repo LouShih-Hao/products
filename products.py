9011a03e38bee906092fc188bba3f54a5f2007ea
Shih-Hao Lou <shihhao.lou@hotmail.com>
2023-10-09 16:02:10 +0800

    add read file

--- products.py
+++ products.py
@@ -23,6 +23,6 @@ for product in products:
 # 寫入檔案
 # 清單列表 最常使用csv檔 通常會使用","做區隔
 with open('products.csv', 'w', encoding = 'utf-8') as f:
-	f.write('商品,價格\n')
+	# f.write('商品,價格\n')
 	for product in products:
 		f.write(product[0] + ',' + product[1] + '\n')
