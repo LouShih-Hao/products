f6ab8aa909b723dd38075254969e87a35bc7f776
Shih-Hao Lou <shihhao.lou@hotmail.com>
2023-10-09 14:59:45 +0800

    final version

--- products.py
+++ products.py
@@ -9,3 +9,8 @@ print(products[0][1])
 
 for product in products:
 	print(product[0], '的價格是', product[1])
+
+# 清單列表 最常使用csv檔 通常會使用","做區隔
+with open('product.csv', 'w') as f:
+	for product in products:
+		f.write(product[0] + ',' + product[1] + '\n')
