88fa5e0daaa3e0461dbc36be241d16a047b2f50f
Shih-Hao Lou <shihhao.lou@hotmail.com>
2023-10-09 15:38:51 +0800

    read .csv file version

--- products.py
+++ products.py
@@ -1,17 +1,24 @@
+# 讀取檔案
 products = []
+with open('products.csv', 'r', encoding = 'utf-8') as f:
+	for line in f:
+		products.append(line.strip().split(','))
+
+print(products)
+
 while True:
 	name = input('請輸入商品名稱:')
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
 	products.append([name, price])
-print(products[0][1])
+# print(products[0][1])
 
 for product in products:
 	print(product[0], '的價格是', product[1])
 
 # 清單列表 最常使用csv檔 通常會使用","做區隔
-with open('product.csv', 'w', encoding = 'utf-8') as f:
+with open('products.csv', 'w', encoding = 'utf-8') as f:
 	f.write('商品,價格\n')
 	for product in products:
 		f.write(product[0] + ',' + product[1] + '\n')
